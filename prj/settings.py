53448102c134f7d08cd377896af52031edc0ddba
lehakot-create <lehakot85@inbox.ru>
2022-03-13 20:38:10 +0400

    fixed static files

--- prj/settings.py
+++ prj/settings.py
@@ -128,9 +128,10 @@ STATIC_URL = 'static/'
 #     BASE_DIR / "static",
 # ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
